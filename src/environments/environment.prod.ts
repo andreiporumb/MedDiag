7a972d9c6e1ebfa945e9af3e32b7e82214d1110e
Andrei Porumbescu <aporumbescu@meddev.ro>
2020-06-28 01:04:23 +0300

    updated production env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAKPV9E0KTS3uQRZOphqdUbvPebnRo3KJ0",
+    authDomain: "meddiag-40e4f.firebaseapp.com",
+    databaseURL: "https://meddiag-40e4f.firebaseio.com",
+    projectId: "meddiag-40e4f",
+    storageBucket: "meddiag-40e4f.appspot.com",
+    messagingSenderId: "195362053320",
+    appId: "1:195362053320:web:12989e337b0262b92799fd",
+    measurementId: "G-9ECDFNVJST"
+  }
 };
